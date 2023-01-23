a3dbf37b41f16e633a279f80efecaf705e0329b3
Juliana Marques <marquesdjuliana@gmail.com>
2023-01-23 10:41:27 -0300

    exerício de fixação01 js

---
+++ fundamentos/secao-3-intro-js/fixacao01.js
@@ -0,0 +1,9 @@
+const myName = "Juliana Marques";
+const birthCity = "Goiânia";
+let birthYear = 1993;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
