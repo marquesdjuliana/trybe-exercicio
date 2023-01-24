38be8268f76cb90019debfbd39bc2f54ef05a66d
Juliana Marques <marquesdjuliana@gmail.com>
2023-01-24 10:33:07 -0300

    adicionando exercício de fixação02

---
+++ fundamentos/secao-3-intro-js/dia-2-array-e-for/fixacao02.js
@@ -0,0 +1,6 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (index = 0; index < index.length; index ++ ) {
+    const elemet = groceryList[index];
+    console.log(groceryList);
+}
