37c2d2b4dc0a3c2fc4459991413a661a3f600b81
Juliana Marques <marquesdjuliana@gmail.com>
2023-01-26 11:55:40 -0300

    adiconando exercício de fixação01 a,b,c,d dia4 seção3

--- fundamentos/secao-3-intro-js/dia-4-objetos-funcoes/fixacao01a.js
+++ fundamentos/secao-3-intro-js/dia-4-objetos-funcoes/fixacao01a.js
@@ -1,4 +1,12 @@
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// Criando objeto e exibindo valores com msg:
+
+let player = {
+  name:'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2, silver: 3,
+  }
+}
+
+console.log( 'A jogadora ' +player['name'] + ' ' + player['lastName'] + ' tem ' + player['age'] + ' anos de idade.');
---
+++ fundamentos/secao-3-intro-js/dia-4-objetos-funcoes/fixacao01b.js
@@ -0,0 +1,14 @@
+// Adicionando valor ao objeto por chave:
+
+let player = {
+  name:'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2, silver: 3,
+  }
+}
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log(player);
