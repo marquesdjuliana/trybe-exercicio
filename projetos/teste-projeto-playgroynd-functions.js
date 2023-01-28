f637fc288c1a59d10aefda6e249a8c3657185f99
Juliana Marques <marquesdjuliana@gmail.com>
2023-01-28 14:59:59 -0300

    adicionando arquivo teste de projeto playgroynd-functions

---
+++ projetos/teste-projeto-playgroynd-functions.js
@@ -0,0 +1,10 @@
+// 1- Crie uma função usando o operador &&
+// 2- Crie uma função que divida uma frase
+// 3- Crie uma função que use concatenação de strings
+// 4- Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
+// 5- Crie uma função que calcula o número de repetições do maior número
+// 6- Crie funções para calcular as áreas de um triângulo e de um retângulo
+// 7- Crie uma função de Caça ao Rato
+// 8- Crie uma função FizzBuzz
+// 9- Crie uma função que Codifique e Decodifique
+// 10- Crie uma função de Lista de Tecnologias
