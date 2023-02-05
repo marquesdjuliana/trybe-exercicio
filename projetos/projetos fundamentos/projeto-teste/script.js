57c525433fef98446727d75fa3581a49ffaf8a72
Juliana Marques <marquesdjuliana@gmail.com>
2023-02-05 15:29:48 -0300

    Adicionando requisito 5

--- projetos/projetos fundamentos/projeto-teste/script.js
+++ projetos/projetos fundamentos/projeto-teste/script.js
@@ -16,17 +16,33 @@ window.onload = () => {
   };
 //Requisito 04: b) uma função para recuperar o valor da função changeColorBtn e adicinar cor aleatória as divs!
   document.getElementById('button-random-color').addEventListener('click', () => {
-    
+    let colorObj = {
+      color2: '',
+      color3: '',
+      color4: '', 
+    };
+
     for (let index = 1; index < 4; index += 1) {
       let color = changeColorBtn();
       let colorDiv = document.getElementById(`color-${index+1}`)
       colorDiv.style.backgroundColor = color;
- 
+      colorObj[`color${index+1}`] = color;
+     
     }
-
+    localStorage.setItem('colorPalette', JSON.stringify(colorObj));
   });
+
+
+  let savedColorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+  let square2 = document.getElementById('color-2');
+  let square3 = document.getElementById('color-3');
+  let square4 = document.getElementById('color-4');
+
+  square2.style.backgroundColor = savedColorPalette['color2'];
+  square3.style.backgroundColor = savedColorPalette['color3'];
+  square4.style.backgroundColor = savedColorPalette['color4'];
+
+
 
 }
   
-
-
