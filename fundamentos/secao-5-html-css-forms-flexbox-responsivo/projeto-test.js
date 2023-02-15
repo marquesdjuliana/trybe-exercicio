ccc1860130c6f2ce0834f7fbb353fbc035829adf
Juliana Marques <marquesdjuliana@gmail.com>
2023-02-15 14:36:03 -0300

    Testando os primeiros requisitos

---
+++ fundamentos/secao-5-html-css-forms-flexbox-responsivo/projeto-test.js
@@ -0,0 +1,13 @@
+
+ const alertPopUp = () => {
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
+
+  if ( email == 'tryber@test.com' && senha == 123456) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+ }
+const button = document.getElementById('entrar');
+button.addEventListener('click', alertPopUp());
