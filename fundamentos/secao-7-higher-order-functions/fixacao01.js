0bbec1f8faa9c4f15cd7720a8b371b9d02dba693
Juliana Marques <marquesdjuliana@gmail.com>
2023-03-02 15:42:33 -0300

    Adiciona exeercícios de fixação em apenas um arquivo js - adiciona fixação - dia 1 seção 7

--- fundamentos/secao-7-higher-order-functions/fixacao01.js
+++ fundamentos/secao-7-higher-order-functions/fixacao01.js
@@ -1,5 +1,44 @@
+// 01: Analisando forEach:
 const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
 
 arrayOfValues.forEach((element, index) => {
   console.log(`O elemento atual é: ${element} e possui o index: ${index}`);
 });
+
+// 02: Use o método forEach para exibir a lista de emails com a seguinte frase: O email ${email} está cadastrado em nosso banco de dados!.
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+emailListInData.forEach((email) => {
+  console.log(`O email ${email} está cadastrado em nosso banco de dados!`)
+});
+
+// 03: Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verificaNumDivisivel = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+
+console.log(verificaNumDivisivel);
+
+//04: Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  return names.find((fivecharacters) => fivecharacters.length === 5);
+};
+
+console.log(findNameWithFiveLetters());
+
+//05 : Utilize o find para encontrar a música com id igual a '31031685', caso ela exista:
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+const findId = musicas.find((idQueQuero) => idQueQuero.id === '31031685' ); // dica:  é necessário acessar a propriedade a ser verificada.
+console.log(findId);
--- fundamentos/secap-7-higher-order-functions/fixacao01.js
+++
@@ -1,7 +0,0 @@
-//Analisando o funcionando do forEtch:
-const arrayOfValues = ['João', 'Lucas', 30, 1.25, { comida: 'Chocolate' }];
-
-arrayOfValues.forEach((element, index, arrayOfValues) => {
-  console.log(`O elemento atual é: ${element} e possui o index: ${index}`);
-  console.log(`A quantidade de elemento é ${arrayOfValues.length}`);
-});
