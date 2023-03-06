ac70bdaf944de3c80dee24dca665ea8d91296959
Juliana Marques <marquesdjuliana@gmail.com>
2023-03-06 12:26:32 -0300

    Adiciona exercício de fixação 02 - dia 3 seção 7

--- fundamentos/secao-7-higher-order-functions/fixacao01.js
+++ fundamentos/secao-7-higher-order-functions/fixacao01.js
@@ -1,3 +1,4 @@
+//Exercício de fixação 01:
 //faça uma função que some todos os números pares do array:
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
@@ -16,3 +17,87 @@ console.log(somarPares(numbers));
 // const sumNumbers = (array) => array.reduce(sum, 0);
 
 // console.log(sumNumbers(numbers)); 
+
+
+// exercício fixação 02:
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+const relatorio = estudantes.map((element) => {
+return {
+  nome: element.nome,
+  materia: element.materias
+  .reduce((acc, curr) => acc.nota > curr.nota ? acc : curr).name
+}});
+console.log(relatorio);
