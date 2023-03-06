438d580a1175365ab2f1c41d571b5dbb9d72c855
Juliana Marques <marquesdjuliana@gmail.com>
2023-03-06 08:12:35 -0300

    Adiciona project unit test

---
+++ projetos/projetos fundamentos/projeto-unit-tests/tests/productDetails.spec.js
@@ -0,0 +1,53 @@
+const productDetails = require('../src/productDetails');
+/*
+  A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara')
+
+  // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  Escreva pelo menos cinco testes para essa função para garantir que a implementação de productDetails está correta.
+
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    let myProduts = productDetails('produto1', 'produto2');
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
+    // Teste se o retorno da função é um array.
+    expect(Array.isArray(myProduts)).toBe(true);
+    // Teste se o array retornado pela função contém dois itens dentro.
+    expect(myProduts).toHaveLength(2);
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect( typeof myProduts[0]).toBe('object');
+    expect( typeof myProduts[1]).toBe('object');
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    let myProduts2 = productDetails('produto3', 'produto4');
+    expect(myProduts).not.toEqual(myProduts2);
+    // Teste se os dois productIds terminam com 123.
+    expect(myProduts2[0].details.productId.endsWith('123')).toBe(true);
+    expect(myProduts2[1].details.productId.endsWith('123')).toBe(true);
+  
+  });
+});
