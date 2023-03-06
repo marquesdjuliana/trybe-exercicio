a6fb74c91557f709c0c686d636e3a202559e4acf
Juliana Marques <marquesdjuliana@gmail.com>
2023-03-06 20:11:29 -0300

    Adiciona resolução do gabarito

--- fundamentos/secao-7-higher-order-functions/dia-3-HOFs-filter-reduce/exercise-mentoria.js
+++ fundamentos/secao-7-higher-order-functions/dia-3-HOFs-filter-reduce/exercise-mentoria.js
@@ -31,17 +31,17 @@ const personagens = [
 // Encontre o personagem que utiliza a habilidade Teleportar
 // Saida Esperada: Mago
 
-// const habilidadeTelepor = personagens.find((personagem) => {
-//   return personagem.habilidades.find((teleportar) => teleportar === 'Teleportar')}).classe;
-// console.log(habilidadeTelepor);
+const habilidadeTelepor = personagens.find((personagem) => {
+  return personagem.habilidades.find((teleportar) => teleportar === 'Teleportar')}).classe;
+console.log(habilidadeTelepor);
 
 
 
 // Verifique se todas as classes tem 3 habilidades
 //Saida Esperada: true
 
-// const confereHabilidades = personagens.every((personagem) =>  personagem.habilidades.length === 3);
-// console.log(confereHabilidades);
+const confereHabilidades = personagens.every((personagem) =>  personagem.habilidades.length === 3);
+console.log(confereHabilidades);
 // Verifique se pelo menos uma classe tem o hp maior que 150 e retorne um valor booleano.
 // Saida Esperada: false
 
@@ -61,11 +61,35 @@ console.log(habilidadesArqueiro());
 
 // Crie uma função que ordene as classes pelo maior ataque e imprima o nome da classe e o ataque:
 // Esperado: ["O ataque do mago é 40", "O ataque do Arqueiro é 20", "O ataque do Guerreiro é 10"]
-
+const ordenarAtaque = (array) => {
+  const ataqueOrdenado = array.sort(
+    (objA, objB) => objB.status.atk - objA.status.atk, // (b - a) decrescente
+  );
+  let finalArray = [];
+  ataqueOrdenado.forEach((personagem) =>
+    finalArray.push(
+      `O ataque do ${personagem.classe} é ${personagem.status.atk}`,
+    ),
+  );
+  return finalArray;
+};
+console.log(ordenarAtaque(personagens));
 
 // Faça uma função que retorne a somatoria de todos os ataques
 // Esperado: "O ataque total é 70"
+const totalAtk = () => {
+  let total = 0;
+  personagens.forEach((classe) => (total += classe.status.atk));
+  return `O Ataque total é ${total}`;
+};
+console.log(totalAtk());
 
+// Tentativa de reduce:
+// const totalAtak = (array) => {
+//   const sumAtak = (acc, curr) => acc + curr;
+//   return array.status.atk.reduce((sumAtak), 0);
+// }
+// console.log(totalAtak(personagens));
 
 //Faça uma função que retorne um novo array com todas as habilidades ordenadas
 // saida esperada: 
@@ -80,3 +104,12 @@ console.log(habilidadesArqueiro());
 //   'Teleportar',
 //   'Tiro flamejante'
 // ]
+const imprimirHabilidades = () => {
+  const arrayHabilidades = [];
+  personagens.forEach((classes) =>
+    classes.habilidades.forEach((poder) => arrayHabilidades.push(poder)),
+  );
+  arrayHabilidades.sort();
+  return arrayHabilidades;
+};
+console.log(imprimirHabilidades());
