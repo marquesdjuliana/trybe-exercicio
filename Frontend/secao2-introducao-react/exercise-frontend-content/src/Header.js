8dc05ccbfa7edd7a7ba6db8d9b619ce4ecf06d89
Juliana Marques <marquesdjuliana@gmail.com>
2023-03-29 15:21:36 -0300

    Reorganizando repositório - parte 3

---
+++ Frontend/secao2-introducao-react/exercise-frontend-content/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1 className="title-content">Conteúdos de Frontend</h1>
+      </section>
+    );
+  }
+}
+
+export default Header;
