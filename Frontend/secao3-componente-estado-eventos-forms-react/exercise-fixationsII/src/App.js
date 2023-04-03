5beb230171452da2c9bede92212c544df9b192c9
Juliana Marques <marquesdjuliana@gmail.com>
2023-04-03 14:58:33 -0300

    Exercise fixationsII - dia 1 - seção 3

---
+++ Frontend/secao3-componente-estado-eventos-forms-react/exercise-fixationsII/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyButtons from './MyButtons';
+// import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (<MyButtons />);
+  }
+};
+
+export default App;
